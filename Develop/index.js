d4778a7084d4d5b216361650ecee1776946df071
Maxx <maxhiga.hawaii@gmail.com>
2020-11-24 16:23:08 -1000

    added .then and writefile

--- Develop/index.js
+++ Develop/index.js
@@ -1,46 +1,55 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is your project title",
-    name: "titleInput",
-  },
-  {
-    type: "input",
-    message: "Description of your project.",
-    name: "descrInput",
-  },
-  {
-    type: "input",
-    message: "Steps for installation requirements.",
-    name: "installInput",
-  },
-  {
-    type: "input",
-    message: "Usage Information.",
-    name: "usageInput",
-  },
-  {
-    type: "input",
-    message: "Contributors on this project.",
-    name: "contribInput",
-  },
-  {
-    type: "input",
-    message: "Guidelines for your project.",
-    name: "guideInput",
-  },
-  {
-    type: "input",
-    message: "Test Instructions.",
-    name: "instrucInput",
-  },
-  {
-    type: "list",
-    message: "License",
-    name: "licenseInput",
-    choices: ["MIT", "IBM", "Apache"],
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your project title",
+      name: "titleInput",
+    },
+    {
+      type: "input",
+      message: "Description of your project.",
+      name: "descrInput",
+    },
+    {
+      type: "input",
+      message: "Steps for installation requirements.",
+      name: "installInput",
+    },
+    {
+      type: "input",
+      message: "Usage Information.",
+      name: "usageInput",
+    },
+    {
+      type: "input",
+      message: "Contributors on this project.",
+      name: "contribInput",
+    },
+    {
+      type: "input",
+      message: "Guidelines for your project.",
+      name: "guideInput",
+    },
+    {
+      type: "input",
+      message: "Test Instructions.",
+      name: "instrucInput",
+    },
+    {
+      type: "list",
+      message: "License",
+      name: "licenseInput",
+      choices: ["MIT", "IBM", "Apache"],
+    },
+  ])
+  .then((res) => {
+    console.log(res);
+
+    fs.writeFile("README.md", readMe, (err) => {
+      if (err) throw err;
+      console.log("complete");
+    });
+  });
